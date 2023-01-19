963c1c289d7311d659330170ed71885dbf7d6472
MauroNatale <mauro.natale00@gmail.com>
2023-01-19 17:03:31 -0300

    licuadoraJS

---
+++ js/motor.js
@@ -0,0 +1,29 @@
+var estadoLicuadora = "apagada";
+var sonidoLicuadora = document.getElementById ("blender-sound");
+var botonLicuadora = document.getElementById("blender-button-sound");
+var licuadora = document.getElementById("blender");
+
+
+function controlarLicuadora (){
+    if (estadoLicuadora == "apagada"){
+        estadoLicuadora = "encendida";
+        hacerBrrBrr();
+        licuadora.classList.add("active");
+        
+    } else{
+        estadoLicuadora = "apagada";
+        hacerBrrBrr();
+        licuadora.classList.remove("active");
+    }
+}
+
+function hacerBrrBrr(){
+    if(sonidoLicuadora.paused){
+        botonLicuadora.play();
+        sonidoLicuadora.play();
+    }else {
+        botonLicuadora.play();
+        sonidoLicuadora.pause();
+        sonidoLicuadora.currentTime = 0;
+    }
+}
